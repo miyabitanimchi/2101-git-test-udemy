67a2ff8de1b21d2096ada0ebb5401529cf88c1d8
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-01-02 16:04:42 -0800

    changed contents

--- 2101git-test-udemy.js
+++ 2101git-test-udemy.js
@@ -1,1 +1,22 @@
-console.log(2 + 3);
+const number1 = 111;
+const number2 = 236;
+const number3 = 5;
+
+const getMax = (a, b, c) => {
+    let max = a;
+
+    if (b > max) {
+        max = b;
+    }
+
+    if (c > max) {
+        max = c;
+    }
+
+    return max;
+
+}
+
+const max = getMax(number1, number2, number3);
+console.log(`The maximum value is ${max}`);
+
